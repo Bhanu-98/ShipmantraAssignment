915d78f191f354ba5115e3f875fecc09a5fc96ff
Bhanu Prakash Oleti <bhanu.prakash.oleti@kyndryl.com>
2022-07-29 17:05:42 +0530

    bug fix

--- GetEstimate.ts
+++ GetEstimate.ts
@@ -5,7 +5,7 @@ const headers = {
     'X-DV-Auth-Token': '89BB911AA612E767A22DD109D81A0CD5BFD8FB39',
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT'
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, OPTIONS'
   }
 
 const instance = axios.create({
