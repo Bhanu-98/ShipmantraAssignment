d6e9078c61b8cd65617619f18f7d9ce121f3be6b
Bhanu Prakash Oleti <bhanu.prakash.oleti@kyndryl.com>
2022-07-29 17:05:27 +0530

    bug fix

--- GetEstimate.ts
+++ GetEstimate.ts
@@ -3,7 +3,7 @@ import axios, { Axios } from "axios";
 
 const headers = {
     'X-DV-Auth-Token': '89BB911AA612E767A22DD109D81A0CD5BFD8FB39',
-    'Access-Control-Allow-Origin': 'https://robotapitest-in.borzodelivery.com',
+    'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
     'Access-Control-Allow-Methods': 'GET, POST, PUT'
   }
